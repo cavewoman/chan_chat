0b0735c80b82e810a0738f7cb37fa525bc09045c
cavewoman <anna.chan365@gmail.com>
2016-02-23 11:40:46 -0800

    Adds nickname functionality and Adds better mobile view

--- index.js
+++ index.js
@@ -2,18 +2,34 @@ var app = require('express')(),
     http = require('http').Server(app),
 		io = require('socket.io')(http);
 
+app.get('/chat', function(req, res) {
+  res.sendFile(__dirname + '/public/chat.html');
+});
+
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket) {
-  io.emit('connection message', 'A user connected.');
+  socket.broadcast.emit('connection message', 'A user connected.');
 	socket.on('disconnect', function() {
-    io.emit('disconnect message', 'A user disconnected.');
+    if (socket.nickname) {
+      socket.broadcast.emit('disconnect message', socket.nickname + ' disconnected');
+    } else
+      socket.broadcast.emit('disconnect message', 'A user disconnected.');
+    }
 	});
 	socket.on('chat message', function(msg) {
-		io.emit('chat message', msg);
+    if (socket.nickname) {
+      io.emit('chat message', '<span class="nickname">' + socket.nickname + '</span> ' + msg);
+    } else {
+      io.emit('chat message', '<span class="nickname">anonymous</span> ' + msg);
+    }
 	});
+  socket.on('add nickname', function(nickname) {
+    socket.nickname = nickname;
+    socket.broadcast.emit('nickname added', socket.nickname);
+  });
 });
 
 http.listen(3000, function() {
