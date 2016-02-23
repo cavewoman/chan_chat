53fec216b11a53475795253da4e25b49df923e23
cavewoman <anna.chan365@gmail.com>
2016-02-23 09:13:07 -0800

    Sends message on connect and disconnect.

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-	console.log('a user connected');
+  io.emit('connection message', 'A user connected.');
 	socket.on('disconnect', function() {
-		console.log('user disconnected');
+    io.emit('disconnect message', 'A user disconnected.');
 	});
 	socket.on('chat message', function(msg) {
 		io.emit('chat message', msg);
